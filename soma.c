1e7b3e26c3b631145762a4a9757851998dc739ba
Polly <112908864+americapolly@users.noreply.github.com>
2023-05-22 17:39:35 -0300

    Create soma.c

---
+++ soma.c
@@ -0,0 +1,12 @@
+//Ler dois numeros e calcula a soma
+main()
+{
+int n1, n2, soma;
+printf("Calcular a soma de dois numeros\n");
+printf("Primeiro valor: ");
+scanf("%d", &n1);
+printf("Segundo valor: ");
+scanf("%d", &n2);
+soma = n1 + n2;
+printf("\n\nA soma dos dois numeros = %d",soma);
+}
